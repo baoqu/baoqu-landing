fdca91c16bc2be8e5ddaef4cd7779826bfedd2f7
andy <andres.gonzalez@kaleidos.net>
2016-05-19 09:51:19 +0200

    css base

--- src/baoqu_landing/templates.clj
+++ src/baoqu_landing/templates.clj
@@ -11,5 +11,10 @@
     [:body
      [:div
       [:h1 "Baoqu Landing"]
+      [:h2 "Let's find out the ideas we share, no matter how many we are"]
+      [:p "Baoqu is an Open Source for horizontal deliberation in massive groups in a whole new way. Yeah, new, belive us."]
+      [:div "Mapping the conversation"]
+      [:div "Trolling resilient"]
+      [:div "Core gamificated"]
       [:div#mail-form]]
      [:script {:type "text/javascript" :src "/assets/js/app.js"}]]]))
