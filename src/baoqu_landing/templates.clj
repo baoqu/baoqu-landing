6d9c21c0769fcde5ffc6bbd34ee42fe8e5fabbc5
andy <andres.gonzalez@kaleidos.net>
2016-05-25 12:46:09 +0200

    initial scale

--- src/baoqu_landing/templates.clj
+++ src/baoqu_landing/templates.clj
@@ -7,7 +7,7 @@
    [:html
     [:head
      [:title "Baoqu"]
-     [:meta {:name "viewport" :content "width=device-width"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
      [:link {:rel "icon" :type "image/png" :href "/assets/img/favicon.png"}]
      [:link {:rel "stylesheet" :href "/assets/css/main.css"}]
      [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Open+Sans"}]]
